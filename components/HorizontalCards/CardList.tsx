50a4facbc9d0c2d054c191a170efb2b8389347ee
alshohid <alshohid1997@gmail.com>
2025-09-16 20:53:15 +0600

    horizontal section mobile responsive done

--- components/HorizontalCards/CardList.tsx
+++ components/HorizontalCards/CardList.tsx
@@ -3,13 +3,19 @@ import { cardImages } from "@/lib/utility/heroUtils"
 import Card from "./Card"
 
 const CardList = ({ isExpanded }: { isExpanded: boolean }) => {
-    return (
-        <div className="flex space-x-4">
-            {cardImages.map((image, index) => (
-                <Card key={index} src={image.src} alt={image.alt} isExpanded={isExpanded} index={index} />
-            ))}
-        </div>
-    )
+  return (
+    <div className="flex flex-wrap gap-4 md:flex-nowrap md:space-x-4">
+      {cardImages.map((image, index) => (
+        <Card
+          key={index}
+          src={image.src}
+          alt={image.alt}
+          isExpanded={isExpanded}
+          index={index}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default CardList
--- components/HorizontalCards/HorizontalCardSection.tsx
+++ components/HorizontalCards/HorizontalCardSection.tsx
@@ -5,22 +5,27 @@ import CardList from "./CardList"
 import TextBlock from "./TextBlock"
 
 const HorizontalCards = () => {
-    const containerRef = useRef<HTMLDivElement>(null)
-    const isExpanded = useCardScroll(containerRef)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const isExpanded = useCardScroll(containerRef)
 
-    return (
-        <section ref={containerRef} className="relative overflow-hidden bg-gray-50">
-            <div className="sticky top-0 h-screen flex items-center">
-                <div className="flex-shrink-0 w-1/2 flex justify-start pl-12">
-                    <CardList isExpanded={isExpanded} />
-                </div>
+  return (
+    <section
+      ref={containerRef}
+      className="relative overflow-hidden bg-gray-50"
+    >
+      <div className="sticky top-0 h-screen flex flex-col md:flex-row items-center md:items-stretch">
+        
+        <div className="flex-shrink-0 w-full md:w-1/2 flex justify-center md:justify-start p-6 md:pl-12">
+          <CardList isExpanded={isExpanded} />
+        </div>
 
-                <div className="flex-1 flex items-center justify-start pl-16 pr-12">
-                    <TextBlock isExpanded={isExpanded} />
-                </div>
-            </div>
-        </section>
-    )
+       
+        <div className="flex-1 w-full flex items-center justify-center md:justify-start px-6 md:pl-16 md:pr-12 mt-6 md:mt-0">
+          <TextBlock isExpanded={isExpanded} />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default HorizontalCards
